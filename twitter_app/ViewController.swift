e9b6d807b7441a456b16b5b61507c7e8aa842fec
adelinka17 <yelemessova.adelina@gmail.com>
2022-03-13 23:38:52 -0400

    Initial Commit

---
+++ twitter_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter_app
+//
+//  Created by Adelina Yelemessova on 3/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
